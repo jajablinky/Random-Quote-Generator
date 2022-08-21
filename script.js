b12b0629ee9ed9c924b22bd241933c321a0c930f
jajablinky <gmillar6@gmail.com>
2022-08-21 16:49:47 -0700

    Version 1.0
    quote array created with object properties including:
    
    quote,
    source,
    citation,
    year
    
    All completed. Now ready for testing.

---
+++ script.js
@@ -0,0 +1,74 @@
+
+const quotes = [
+    {
+      quote: "I don't think people are going to talk in the future. They're going to communicate through eye contact, body language, emojis, signs.",
+      source: 'Kanye West',
+    },
+    {
+      quote: "If I'm traveling, I'll pack socks in my bag - really cute furry ones.",
+      source: 'Nicki Minaj',
+    },
+    {
+      quote: "Focused, hard work is the real key to success. Keep your eyes on the goal, and just keep taking the next step towards completing it. If you aren't sure which way to do something, do it both ways and see which works better.",
+      source: 'John Carmack',
+      year:'2022'
+    },
+    {
+      quote: "I have no interest in going to Egypt and seeing the pyramids. I'm just not that kind of dude.",
+      source: 'Gary Vaynerchuk',
+    },
+    {
+      quote: "The team comes first.",
+      source: 'Simone Biles',
+    },
+    {
+      quote: "Showing weakness and showing fear and letting people in was a huge part of my comeback.",
+      source: 'Mardy Fish',
+      citation:'Untold: Breaking Point '
+    },
+    {
+      quote: "I don't understand why people would want to get rid of pigeons. They don't bother no one.",
+      source: 'Mike Tyson',
+      known_for:'Pro Boxer'
+    },
+    {
+      quote: "The world can't tell you who you are. You've just got to figure out who you are and be there, for better or worse.",
+      source: 'Dave Chapelle',
+      known_for:'Comedian'
+    },
+    {
+      quote: "I feel like, when people realize that they are the only person they need to impress, everybody's life will be a lot smoother.",
+      source: 'Megan Thee Stallion',
+      known_for:'Musician'
+    },
+    {
+      quote: "I feel like, when people realize that they are the only person they need to impress, everybody's life will be a lot smoother.",
+      source: 'Megan Thee Stallion',
+      known_for:'Musician'
+    },
+    {
+      quote: "I think I'm cool. That's all that matters.",
+      source: 'Tyler, The Creator',
+      known_for:'Musician'
+    },
+    {
+      quote: "I think I'm cool. That's all that matters.",
+      source: 'Tyler, The Creator',
+      known_for:'Musician'
+    },
+    {
+      quote: "You can be gorgeous at thirty, charming at forty, and irresistible for the rest of your life.",
+      source: 'Coco Chanel',
+      known_for:'Fashion Designer'
+    },
+    {
+      quote: "There comes a time in your life when you focus solely on what you believe is right, regardless of what everybody else is doing.",
+      source: 'Alexander Mcqueen',
+      known_for:'Fashion Designer'
+    },
+    {
+      quote: "I just feel my best when I'm all glammed up.",
+      source: 'Kim Kardashian',
+      known_for:'Influencer'
+    }
+  ];
